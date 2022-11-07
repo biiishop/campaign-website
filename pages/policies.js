db7c8142a531dc5c7714e3b840a28c4ea9b09a11
biishop <orlin.dyankov@gmail.com>
2022-11-06 22:19:03 -0500

    sanity

--- pages/policies.js
+++ pages/policies.js
@@ -1,14 +1,37 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/layout';
+import { createClient } from "next-sanity";
 
-export default function Policies() {
+export default function Policies({ policies }) {
 	return (
 		<Layout>
 			<Head>
 				<title>First Post</title>
 			</Head>
-			<p className='body-text'>Write Here</p>
+			<ul>
+            {policies.map((policy) => (
+              <li key={policy._id}>{policy?.policies}</li>
+            ))}
+          </ul>
 		</Layout>
 	);
+}
+
+const client = createClient({
+  projectId: "dr264hba",
+  dataset: "production",
+  apiVersion: "2022-03-25",
+  useCdn: false
+});
+
+
+export async function getStaticProps() {
+  const policies = await client.fetch(`*[_type == "policies"]`);
+  
+  return {
+    props: {
+      policies
+    }
+  };
 }
--- pages/socials.js
+++ pages/socials.js
@@ -1,15 +1,54 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/layout';
+import { createClient } from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url'
+import React from 'react'
 
-export default function Socials() {
+
+export default function Socials({ socials }) {
 	return (
 		<Layout>
 			<h1 className='title'>Socials</h1>
 			<div className='grid'>
-				<a href="" className='card'>Joint Instagram account</a>
-				<a href="" className='card'>Facebook Accuont</a>
+				{socials.map((social) => (
+					<a href={social.url} target="_" className='card'>
+						<h3 className='title'>
+							{social.title}
+						</h3>
+						<center>
+							<img src={urlFor(social.image).width(100).height(100).url()} alt="A picture"/>
+							<p>
+								{social.description}
+							</p>
+						</center>
+					</a>
+            	))}
 			</div>
 		</Layout>
 	);
+}
+
+const client = createClient({
+	projectId: "dr264hba",
+	dataset: "production",
+	apiVersion: "2022-03-25",
+	useCdn: false
+});
+
+
+export async function getStaticProps() {
+	const socials = await client.fetch(`*[_type == "socials"]`);
+	
+	return {
+		props: {
+			socials
+		}
+	};
+}
+
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+  return builder.image(source)
 }
