a9b3c6a4f6fa3dad2301fcc0f1cf3d6cebd49dde
biishop <orlin.dyankov@gmail.com>
2022-11-06 19:11:15 -0500

    bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/global.css';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
